b6310c5d42d83c576dcb3e9f2971ab4cf223a0aa
anys34 <anyeseong34@gmail.com>
2024-02-17 17:15:49 +0900

    init

---
+++ src/main/java/com/anys34/dreaming/DreamingApplication.java
@@ -0,0 +1,13 @@
+package com.anys34.dreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anys34/dreaming/DreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anys34.dreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
